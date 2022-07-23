15cfb7a9eab82293bf506d62012dd00861a1aaf9
Sam Warner <its.sam.warner@gmail.com>
2022-07-23 12:22:30 -0400

    Add shot component base and asset files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Shot shot_count=3 />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import Shot from './components/Shot.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    Shot,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/Shot.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>Shot {{ shot_count }}</h1>
+  <div class="paper_body">i am in the shot component</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    shot_count: Number,
+  },
+})
+export default class Shot extends Vue {
+  shot_count!: number;
+}
+</script>
+
+<style scoped></style>
