72906e280c52e81f1a6038cbf31ab7353d69dab0
Sam Warner <its.sam.warner@gmail.com>
2022-07-23 13:52:49 -0400

    Add possible base styling for component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
+  <div class="taped_photograph">
+    <img alt="tape" class="tape" src="./assets/ancient_removed_tape.gif" />
+    <img alt="back of old photograph" width="400" src="./assets/paper_old_back_of_photograph.png" />
+  </div>
+  
   <Shot shot_count=3 />
 </template>
 
@@ -22,5 +27,15 @@ export default class App extends Vue {}
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.taped_photograph {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.tape {
+  margin-bottom: -2.5em;
+  z-index: 1;
 }
 </style>
